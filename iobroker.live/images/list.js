b08fc623e761e74e805ef2c89e3c9b8de3967f33
GermanBluefox <dogafox@gmail.com>
2019-02-19 21:05:57 +0100

    (bluefox) add download images

---
+++ iobroker.live/images/list.js
@@ -0,0 +1,87 @@
+var images = [
+    {
+        file: 'ioBroker_Image_RPi_2-3_20190104_stretch.zip',
+        linux: 'stretch',
+        date: '20190104',
+        device: 'Raspberry Pi 2-3',
+        details: 'ioBroker minimal Image<br><ul><li>nodejs 8.15.0</li><li>npm 6.4.1</li></ul>',
+        picture: 'raspberry3.png',
+        user: 'pi',
+        password: 'raspberry',
+        info: 'Experimental - kein Readme!<br/>Bitte Feedback im Forum'
+    },
+    {
+        file: 'ioBroker_Image_Tinker_20181216_stretch.zip',
+        linux: 'stretch',
+        date: '20181216',
+        device: 'Tinker board',
+        details: 'ioBroker minimal Image<br><ul><li>nodejs 8.14.0</li><li>npm 6.4.1</li></ul>',
+        picture: 'tinkerboard.png',
+        user: 'root',
+        password: '1234',
+        info: 'noch kein Readme!<br/>Testing für neue Installationsroutine, bitte Feedback im Forum.'
+    },
+    {
+        file: 'ioBroker_Image_Pine64_20181215_Ubuntu.zip',
+        linux: 'xenial',
+        date: '20181215',
+        device: 'Pine64',
+        details: 'ioBroker minimal Image<br><ul><li>nodejs 8.11.1</li><li>npm 6.4.1</li></ul>',
+        picture: 'rock64.png',
+        user: 'root',
+        password: '1234',
+        info: 'nand-sata-install funktioniert wieder<br/>jedoch nightly build!',
+        readme: 'http://www.iobroker.net/docu/?page_id=8071&lang=de'
+    },
+    {
+        file: 'ioBroker_all_in_one_Image_RPi_2-3_20180419_stretch.zip',
+        linux: 'stretch',
+        date: '20180419',
+        device: 'Raspberry Pi 2-3',
+        details: 'all in one',
+        picture: 'raspberry3.png',
+        user: '',
+        password: '',
+        info: ''
+    },
+    {
+        file: 'ioBroker_Image_RPi_2-3_20180401_stretch.zip',
+        linux: 'stretch',
+        date: '20180401',
+        device: 'Raspberry Pi 2-3',
+        details: '-',
+        picture: 'raspberry3.png'
+    },
+    {
+        file: 'ioBroker_Image_Rock64_20180419_stretch.zip',
+        linux: 'stretch',
+        date: '20180419',
+        device: 'Rock64',
+        details: '-',
+        picture: 'rock64.png'
+    },
+    {
+        file: 'ioBroker_Image_Cubietruck_20171212_Stretch.zip',
+        linux: 'stretch',
+        date: '20171212',
+        device: 'Cubietruck',
+        details: '-',
+        picture: 'cubieboard.png'
+    },
+    {
+        file: 'ioBroker_Image_OPiplus2E_20171212_xenial_pivCCU.zip',
+        linux: 'xenial',
+        date: '20171212',
+        device: 'Tinker board',
+        details: 'pivCCU',
+        picture: 'tinkerboard.png'
+    },
+    {
+        file: 'ioBroker_Image_Tinker_20171214_xenial.zip',
+        linux: 'xenial',
+        date: '20171214',
+        device: 'Tinker board',
+        details: '-',
+        picture: 'tinkerboard.png'
+    }
+];
